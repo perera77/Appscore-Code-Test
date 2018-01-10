366a00207baeb4b015bc813727acb6627a776765
perera77 <suraj.perera@tpg.com.au>
2018-01-10 14:30:43 +1100

    Implements pagination for the simple search page. Also fixed following issues: - gender display - birth place mapping - case sensitivity of search key

--- AppscoreAncestry/Controllers/AncestryController.cs
+++ AppscoreAncestry/Controllers/AncestryController.cs
@@ -26,6 +26,8 @@ namespace AppscoreAncestry.Controllers
             else if (!male && female)
                 genderKey = "F";
 
+            string searchKey = key.ToUpper();
+
             IEnumerable<Person> selected;
             try
             {
@@ -33,17 +35,25 @@ namespace AppscoreAncestry.Controllers
                 if (string.IsNullOrEmpty(genderKey))
                 {
                     selected = (from p in people
-                                where p.name.Contains(key)
+                                where p.name.ToUpper().Contains(searchKey)
                                 select p);
                 }
                 else
                 {
                     selected = (from p in people
-                                where p.name.Contains(key) && p.gender == genderKey
+                                where p.name.ToUpper().Contains(searchKey) && p.gender == genderKey
                                 select p);
                 }
-                if(selected.Count() > 0)
-                    return Ok(selected.Take(10)); // only returns the first ten entries - TODO implement paging
+                if (selected.Count() > 0)
+                {
+                    var page = selected.Skip(10 * (pageRequired - 1)).Take(10); // only returns the first ten entries - TODO implement paging
+
+                    return Ok(new {
+                        people = page.Select(p => new { id = p.id, name = p.name, gender = p.gender == "M" ? "Male" : "Female", birthplace = place_of_birth(p.place_id) }),
+                        page = pageRequired,
+                        total_pages = (selected.Count() + 9) / 10
+                    });
+                }
                 else
                     return NotFound();
             }
@@ -52,6 +62,13 @@ namespace AppscoreAncestry.Controllers
                 return NotFound();
             }
             
+        }
+
+        private string place_of_birth(int id)
+        {
+            IEnumerable <Place> places = DataStore.Instance.Places;
+            var select = places.FirstOrDefault(place => place.id == id);
+            return select != null ? select.name : "";
         }
     }
 }
